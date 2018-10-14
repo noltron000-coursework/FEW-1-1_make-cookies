07045fd437c49671fdd9f1849445ac3f3200a379
Nolan Kovacik <noltron000@gmail.com>
2018-10-14 00:11:19 -0700

    refactor > index + scripts

--- scripts.js
+++ scripts.js
@@ -1,46 +1,66 @@
-//declare default variables
-let cookieCount = 0;
-let clickPower = 1;
-let clickPowerPriceAmount = 50;
-let clickPowerLevelNumber = 1;
+  /*----------------*/
+ /* Cookie Clicker */
+/*----------------*/
 
-//declare DOM variables
+// defaults
+let cookieCount = 0
+
+// DOM declarations
 let cookieCounter = document.getElementById('cookie-counter');
 let cookieClicker = document.getElementById('cookie-clicker');
-let buyClickPower = document.getElementById('buy-click-power');
-let clickPowerPrice = document.getElementById('click-power-price');
-let clickPowerLevel = document.getElementById('click-power-level');
-let clickPowerMultiple = document.getElementById('click-power-multiple');
 
-//everytime a user clicks the button, their cookies are increased by the value of their clickPower.
+// everytime a user clicks the button, their cookies are increased by the value of their clickPower.
 cookieClicker.addEventListener("click", function() {
 	cookieCount = cookieCount + clickPower;
 	refreshCookieCount()
 })
 
-//Buy click power
+// refresh cookie count
+let refreshCookieCount = function() {
+	cookieCounter.innerHTML = cookieCount;
+}
+
+
+  /*----------------*/
+ /* Clicker Powers */
+/*----------------*/
+
+// declare default variables
+let clickPower = 1;
+let clickPowerPriceAmount = 50;
+let clickPowerLevelNumber = 1;
+
+// declare DOM variables
+let buyClickPower = document.getElementById('buy-click-power');
+let clickPowerPrice = document.getElementById('click-power-price');
+let clickPowerLevel = document.getElementById('click-power-level');
+let clickPowerMultiple = document.getElementById('click-power-multiple');
+
+// buy click power
 buyClickPower.addEventListener("click", function() {
 	if (cookieCount >= clickPowerPriceAmount) {
+
 		//subtract cookies from the price of the item
 		cookieCount -= clickPowerPriceAmount;
 		refreshCookieCount()
+
 		//Upgrade power level
 		clickPowerLevelNumber += 1;
+
 		//Update click price
-		clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 1.33);
+		clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 4 / 3);
+
 		//Update click power
 		clickPower += 1;
+
 		//refresh shop item
 		refreshPowerClick();
 	}
 })
 
-let refreshCookieCount = function() {
-	cookieCounter.innerHTML = cookieCount;
-}
-
+// refresh click power
 let refreshPowerClick = function() {
-	clickPowerLevel.innerHTML = clickPowerLevelNumber
+	clickPowerLevel.innerHTML = clickPowerLevelNumber;
 	clickPowerPrice.innerHTML = clickPowerPriceAmount;
 	clickPowerMultiple.innerHTML = clickPower;
 }
